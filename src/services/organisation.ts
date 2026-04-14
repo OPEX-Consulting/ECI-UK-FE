d13c01b99046d73c7a2a038294a0e5c130b4e2c3
John Uchenna <okechukwuchenna@gmail.com>
2026-04-14 11:43:16 +0100

    school type int

--- src/services/organisation.ts
+++ src/services/organisation.ts
@@ -1,5 +1,5 @@
 import api from "@/lib/api";
-import { ApiOrganisation } from "@/types/organisation";
+import { ApiOrganisation, ApiSchoolType } from "@/types/organisation";
 
 
 export const getOrganisations = async (skip = 0, limit = 100): Promise<ApiOrganisation[]> => {
@@ -8,3 +8,13 @@ export const getOrganisations = async (skip = 0, limit = 100): Promise<ApiOrgani
   });
   return response.data;
 };
+
+export const getSchoolTypes = async (skip = 0, limit = 100): Promise<ApiSchoolType[]> => {
+  console.log("Fetching school types with params:", { skip, limit });
+  const response = await api.get<ApiSchoolType[]>("/admin/school-types", {
+    params: { skip, limit }
+  });
+  console.log("School types API response:", response.data);
+  return response.data;
+};
+
