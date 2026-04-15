50479d2638eac31348a8b373162599fdc2cbbdb7
ernie1234 <anumahjoshuaeneye@gmail.com>
2026-04-15 17:20:00 +0100

    chore: update .gitignore to include .env and enable BASE_URL logging in api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL = import.meta.env.VITE_LOCAL_API_URL;
 
-// console.log("BASE_URL: ", BASE_URL);
+console.log("BASE_URL: ", BASE_URL);
 
 const api = axios.create({
   baseURL: BASE_URL,
